0cbe7977b4e88d42432e080f40c8e6df29134e2b
Jack <jack.doherty@hotmail.co.uk>
2024-04-23 21:02:09 +0100

    Added interface for FizzBuzzCalculators, implemented a Fizz Calculator.

---
+++ FizzBuzz/Calculators/FizzCalculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz.Calculators
+{
+    internal class FizzCalculator : IFizzBuzzCalculator
+    {
+
+        private int _divisor = 3;
+        private string _newValue = "Fizz";
+
+        public string CheckValue(int i)
+        {
+            if(i % _divisor == 0)
+            {
+                return _newValue;
+            }
+
+            return "";
+        }
+    }
+}
--- FizzBuzz/FizzBuzzService.cs
+++ FizzBuzz/FizzBuzzService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FizzBuzz.Calculators;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,29 @@ namespace FizzBuzz
 {
     public class FizzBuzzService
     {
+        private List<IFizzBuzzCalculator> _calculators;
+
+        public FizzBuzzService()
+        {
+            _calculators = new List<IFizzBuzzCalculator>();
+            _calculators.Add(new FizzCalculator());
+        }
+
         public string GetFizzBuzzValue(int i)
         {
-            throw new NotImplementedException();
+            string fizzBuzzValue = "";
+
+            foreach(IFizzBuzzCalculator calculator in _calculators)
+            {
+                fizzBuzzValue += calculator.CheckValue(i);
+            }
+
+            if(fizzBuzzValue.Length == 0)
+            {
+                fizzBuzzValue = i.ToString();
+            }
+
+            return fizzBuzzValue;
         }
     }
 }
